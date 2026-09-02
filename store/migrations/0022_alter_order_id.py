27c3afad8e5978405e25a41177586211f4ee2bb5
Akshay H <akshayhuliyaru2005@gmail.com>
2026-09-02 21:29:42 +0530

    Fix PostgreSQL migration error for order id

--- store/migrations/0022_alter_order_id.py
+++ store/migrations/0022_alter_order_id.py
@@ -11,9 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='order',
-            name='id',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
-        ),
     ]
--- store/migrations/0023_alter_order_id.py
+++ store/migrations/0023_alter_order_id.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='order',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
-        ),
     ]
